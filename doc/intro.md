842309db1082a516eca30981a498b1af9338f211
Bill La Forge <laforge49@gmail.com>
2022-09-28 06:33:26 -0400

    idea

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spark2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/spark2/core_test.clj
@@ -0,0 +1,7 @@
+(ns spark2.core-test
+  (:require [clojure.test :refer :all]
+            [spark2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
