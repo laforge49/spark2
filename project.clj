cb89701e00527503219a7004c43976272f777bf3
Bill La Forge <laforge49@gmail.com>
2022-10-01 11:54:20 -0400

    It runs.

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject spark2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The AllSpark of Gems"
+  :url "https://github.com/laforge49/spark2"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns spark2.core})
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns spark2.core}
+  :main ^:skip-aot spark2.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/spark2/core.clj
+++ src/spark2/core.clj
@@ -1,6 +1,6 @@
 (ns spark2.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (println "*** Begin Run ***")
+  (println "*** End Run ***"))
