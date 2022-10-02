c03fd786fb0eaca797bf76dbdd17d2d76cad6665
Bill La Forge <laforge49@gmail.com>
2022-10-02 19:25:10 -0400

    simplify

--- src/spark/core.clj
+++ src/spark/core.clj
@@ -2,25 +2,14 @@
   (:require
     [clojure.stacktrace :as stacktrace]
     [spark.boot :as boot]
-    [spark.debug :as debug]
-    [spark.eval :as eval]
-    [spark.kws :as kws]
-    [spark.parse :as parse]
     )
   (:gen-class))
 
 (defn -main
   [& args]
-  (println)
+  (println "Start")
   (try
-    (let [env boot/initial-env]
-      (boot/create-gems env)
-      (eval/gem-eval (into env {:param/gem-kw  :gem/facets-schema
-                                :param/role-kw :roles/test
-                                :param/request :debug/ribbit-request}))
-      (eval/gem-eval (into env {:param/gem-kw  :gem/facets-schema
-                                :param/role-kw :roles/test
-                                :param/request :debug/print-value})))
+    (let [env (boot/create-gems {} {})])
     (println "Fin")
     (catch Exception e
       (stacktrace/print-stack-trace e))))
--- src/spark2/core.clj
+++
@@ -1,6 +0,0 @@
-(ns spark2.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
