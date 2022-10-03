7df260a9bb1af5ae05af7da2ab7328771ce13633
Bill La Forge <laforge49@gmail.com>
2022-10-03 10:46:16 -0400

    asString=

--- src/spark/pretty.clj
+++ src/spark/pretty.clj
@@ -1,16 +1,29 @@
 (ns spark.pretty
   (:require [clojure.string :as string]))
 
+(defn blanks
+  [count]
+  (string/join (repeat count " ")))
+
 (defn asString=
   [gems params]
-  [gems [(:value params)]])
+  (let [prefix (get params :prefix "")
+        unprefix (blanks (count prefix))
+        value (:value params)]
+    [gems [value]]))
 
-(defn debug
+(defn debug=
   [gems params]
   (let [path (:path params)
         value (get-in gems path)
-        params (into params {:value value})
+        params (into params {:value value
+                             :prefix ""})
         [gems lines] (asString= gems params)]
-    (println (string/join "\n" (into ["---"]
-                                     lines)))
+    [gems (string/join "\n" (into ["---"]
+                                     lines))]))
+
+(defn debug
+  [gems params]
+  (let [[gems txt] (debug= gems params)]
+    (println txt)
     gems))
