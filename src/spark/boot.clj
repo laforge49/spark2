ad232e36588b2b4e240719f2664251748ecad935
Bill La Forge <laforge49@gmail.com>
2022-10-02 20:30:45 -0400

    stub create-gem

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -1,6 +1,13 @@
 (ns spark.boot)
 
+(defn create-gem
+  [gems params]
+  (let [gem-kw (:gem-kw params)
+        ]
+    gems))
+
 (defn create-gems
-  [gems parms]
-  (println "Blip!")
-  gems)
+  [gems params]
+  (let [params (into params
+                     {:gem-kw :gem/facets-schema})]
+    gems))
