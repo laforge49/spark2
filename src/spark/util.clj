4f58dda097d567a19d8e9ff83a6bd5ac547f4cbc
Bill La Forge <laforge49@gmail.com>
2022-10-05 11:30:59 -0400

    rth rassoc

--- src/spark/util.clj
+++ src/spark/util.clj
@@ -4,3 +4,15 @@
 (defn blanks
   [count]
   (string/join (repeat count " ")))
+
+(defn rth
+  [vec index]
+  (if (< index 0)
+    (nth vec (- (count vec) index))
+    (nth vec index)))
+
+(defn rassoc
+  [vec index value]
+  (if (< index 0)
+    (assoc vec (- (count vec) index) value)
+    (assoc vec index value)))
