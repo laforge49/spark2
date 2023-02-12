ff18cefba6346f02de0e7580b310eccd4ca7e53b
Bill La Forge <laforge49@gmail.com>
2023-02-12 18:40:07 -0500

    revised api again

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -2,22 +2,18 @@
   (:require [spark.pretty :as pretty]))
 
 (defn create-gem
-  [gems params]
-  (let [gem-kw (:gem-kw params)
-        gem
-        {:facet/id          gem-kw
-         :facet/descriptors {:descriptors/roles {}}}
-        gems
-        (assoc gems gem-kw gem)
-        ]
-    [gems params]))
+  ([gems params]
+   (create-gem gems params (:boot/gem-kw params)))
+  ([gems params gem-kw]
+   (let [gem
+         {:facet/id          gem-kw
+          :facet/descriptors {:descriptors/roles {}}}
+         gems
+         (assoc gems gem-kw gem)]
+     [gems params])))
 
 (defn create-gems
   [gems params]
-  (let [local (into params
-                     {:gem-kw :gem/facets-schema})
-        [gems local] (create-gem gems local)
-        [gems local] (pretty/debug gems (into local
-                                      {:path [:gem/facets-schema]}))
-        ]
+  (let [[gems local] (create-gem gems params :gem/facets-schema)
+        [gems local] (pretty/debug gems local [:gem/facets-schema])]
     [gems params]))
--- src/spark/pretty.clj
+++ src/spark/pretty.clj
@@ -3,28 +3,50 @@
             [spark.util :as util]))
 
 (defn asString=
-  [gems params]
-  (let [prefix (get params :prefix "")
-        unprefix (util/blanks (count prefix))
-        value (:value params)]
-    (let [lines
-          (cond
-            :else
-            [(string/join [prefix (pr-str value)])])]
-      [gems (into params {:pretty/lines lines})])))
+  ([gems params]
+   (asString= gems params
+              (:pretty/prefix params)
+              (:pretty/value params)))
+  ([gems params prefix value]
+   (let [unprefix (util/blanks (count prefix))]
+     (let [lines
+           (cond
+             (map? value)
+             (reduce
+               (fn [lines [k v]]
+                 (if (map? v)
+                   (let [lines
+                         (conj lines
+                               (string/join [prefix (pr-str k) ":"]))
+                         pre
+                         (string/join prefix "  ")
+                         lines
+                         (conj lines
+                               (string/join [pre (pr-str v)]))]
+                     lines)
+                   (conj lines
+                         (string/join [prefix (pr-str k) ": " (pr-str v)]))))
+               []
+               value)
+             :else
+             [(string/join [prefix (pr-str value)])])]
+       [gems (into params {:pretty/lines lines})]))))
 
 (defn debug=
-  [gems params]
-  (let [path (:path params)
-        value (get-in gems path)
-        local (into params {:value  value
-                            :prefix ""})
-        [gems local] (asString= gems local)
+  ([gems params]
+   (debug= gems params
+           (:pretty/path params)))
+  ([gems params path]
+  (let [value (get-in gems path)
+        [gems local] (asString= gems params "" value)
         txt (string/join "\n" (into ["---"] (:pretty/lines local)))]
-    [gems (into params {:pretty/txt txt})]))
+    [gems (into params {:pretty/txt txt})])))
 
 (defn debug
-  [gems params]
-  (let [[gems local] (debug= gems params)]
+  ([gems params]
+   (debug gems params
+          (:pretty/path params)))
+  ([gems params path]
+  (let [[gems local] (debug= gems params path)]
     (println (:pretty/txt local))
-    [gems params]))
+    [gems params])))
