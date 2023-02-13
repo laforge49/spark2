d4e81146de39d4fbfdf061c50d37f1769f6ca99f
Bill La Forge <laforge49@gmail.com>
2023-02-13 17:19:31 -0500

    Update access.clj

--- src/spark/access.clj
+++ src/spark/access.clj
@@ -5,7 +5,9 @@
    (gem-path gems params
              (:access/gem-name params)))
   ([gems params gem-name]
-   [gems (into params {:access/path [gem-name]})]))
+   (if (keyword? gem-name)
+     [gems (into params {:access/path [gem-name]})]
+     (throw (Exception. (str ":access/gem-name is not a keyword: " (pr-str gem-name)))))))
 
 (defn descriptors-path
   ([gems params]
@@ -39,7 +41,10 @@
          (roles-path gems params gem-name)
          path (:access/path local)
          path (conj path role-name)]
-     [gems (into params {:access/path path})])))
+     (if (keyword? role-name)
+       [gems (into params {:access/path path})]
+       (throw (Exception. (str ":access/role-name is not a keyword: "
+                               (pr-str role-name))))))))
 
 
 (defn requests-path
@@ -66,4 +71,7 @@
          (requests-path gems params gem-name role-name)
          path (:access/path local)
          path (conj path request-name)]
-     [gems (into params {:access/path path})])))
+     (if (keyword? request-name)
+       [gems (into params {:access/path path})]
+       (throw (Exception. (str ":access/request-name is not a keyword: "
+                               (pr-str request-name))))))))
