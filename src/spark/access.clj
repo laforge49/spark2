eb37287e9d9feec8dbfd94a7fe4012f15fb66ae6
Bill La Forge <laforge49@gmail.com>
2023-02-13 17:06:07 -0500

    Update access.clj

--- src/spark/access.clj
+++ src/spark/access.clj
@@ -2,13 +2,15 @@
 
 (defn gem-path
   ([gems params]
-   (gem-path gems params (:access/gem-name params)))
+   (gem-path gems params
+             (:access/gem-name params)))
   ([gems params gem-name]
    [gems (into params {:access/path [gem-name]})]))
 
 (defn descriptors-path
   ([gems params]
-   (descriptors-path gems params (:access/gem-name params)))
+   (descriptors-path gems params
+                     (:access/gem-name params)))
   ([gems params gem-name]
    (let [[gems local]
          (gem-path gems params gem-name)
@@ -18,7 +20,8 @@
 
 (defn roles-path
   ([gems params]
-   (roles-path gems params (:access/gem-name params)))
+   (roles-path gems params
+               (:access/gem-name params)))
   ([gems params gem-name]
    (let [[gems local]
          (descriptors-path gems params gem-name)
@@ -28,30 +31,39 @@
 
 (defn role-path
   ([gems params]
-   (role-path gems params (:access/role-name params)))
-  ([gems params role-name]
+   (role-path gems params
+              (:access/gem-name params)
+              (:access/role-name params)))
+  ([gems params gem-name role-name]
    (let [[gems local]
-         (roles-path gems params)
+         (roles-path gems params gem-name)
          path (:access/path local)
          path (conj path role-name)]
      [gems (into params {:access/path path})])))
 
 
 (defn requests-path
-  [gems params]
-  (let [[gems local]
-        (role-path gems params)
-        path (:access/path local)
-        path (conj path :role/requests)]
-    [gems (into params {:access/path path})]))
+  ([gems params]
+   (requests-path gems params
+                  (:access/gem-name params)
+                  (:access/role-name params)))
+  ([gems params gem-name role-name]
+   (let [[gems local]
+         (role-path gems params gem-name role-name)
+         path (:access/path local)
+         path (conj path :role/requests)]
+     [gems (into params {:access/path path})])))
 
 
 (defn request-path
   ([gems params]
-   (role-path gems params (:access/request-name params)))
-  ([gems params request-name]
+   (request-path gems params
+                 (:access/gem-name params)
+                 (:access/role-name params)
+                 (:access/request-name params)))
+  ([gems params gem-name role-name request-name]
    (let [[gems local]
-         (requests-path gems params)
+         (requests-path gems params gem-name role-name)
          path (:access/path local)
          path (conj path request-name)]
      [gems (into params {:access/path path})])))
