a1f34621246f0f502b85b184894b796ced6004f4
Bill La Forge <laforge49@gmail.com>
2023-02-13 14:53:44 -0500

    Update access.clj

--- src/spark/access.clj
+++ src/spark/access.clj
@@ -9,14 +9,25 @@
 (defn descriptors-path
   [gems params]
   (let [[gems local]
-        (gem-path gems param)
-        path (:access/path local)]
-    [gems (into params {:access/path (conj path :facet/descriptors)})]))
+        (gem-path gems params)
+        path (:access/path local)
+        path (conj path :facet/descriptors)]
+    [gems (into params {:access/path path})]))
 
 (defn roles-path
   [gems params]
   (let [[gems local]
-        (descriptors-path gems param)
-        path (:access/path local)]
-    [gems (into params {:access/path (conj path :descriptors/roles)})]))
+        (descriptors-path gems params)
+        path (:access/path local)
+        path (conj path :descriptors/roles)]
+    [gems (into params {:access/path path})]))
 
+(defn role-path
+  ([gems params]
+   (role-path gems params (:access/role-name params)))
+  ([gems params role-name]
+   (let [[gems local]
+         (roles-path gems params)
+         path (:access/path local)
+         path (conj path role-name)]
+     [gems (into params {:access/path path})])))
